ad93fedcf5c24ff18c899d3f2ab4d445cd9c623c
Alexander Makarof <makarofalex@gmail.com>
2020-02-25 15:25:34 +0300

    save control point, before multiple grird

--- src/Settings/CoinHeader.jsx
+++ src/Settings/CoinHeader.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import {CoinHeaderGridStyled, CoinSymbol } from '../Shared/AppStyle'; 
+import React from "react";
+import { CoinHeaderGridStyled, CoinSymbol } from "../Shared/AppStyle";
 
-export default function CoinHeader({name, symbol}) {
+export default function CoinHeader({ name, symbol }) {
   return (
     <CoinHeaderGridStyled>
       <div>{name}</div>
       <CoinSymbol>{symbol}</CoinSymbol>
-      
     </CoinHeaderGridStyled>
-  )
+  );
 }
--- src/Settings/CoinTile.jsx
+++ src/Settings/CoinTile.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import {AppContext} from '../App/AppProvider';
-import {SelectableTile} from '../Shared/Tile'
-import CoinHeader from './CoinHeader'
+import React from "react";
+import { AppContext } from "../App/AppProvider";
+import { SelectableTile } from "../Shared/Tile";
+import CoinHeader from "./CoinHeader";
 
-export default function({coinKey}) {
+import CoinImage from "../Shared/CoinImage";
+
+export default function CoinTile({ coinKey }) {
   return (
     <AppContext.Consumer>
-        {({coinList})=> {
-          let coin = coinList[coinKey];
-          const TileClass = SelectableTile;
-          return (
-              <TileClass> 
-                <CoinHeader 
-                name={coin.CoinName}
-                symbol={coin.Symbol}  
-                />
-                {console.log(coinList)}
-              </TileClass>
-          )
-        }}
+      {({ coinList }) => {
+        let coin = coinList[coinKey];
+        const TileClass = SelectableTile;
+        return (
+          <TileClass>
+            <CoinHeader name={coin.CoinName} symbol={coin.Symbol} />
+            <CoinImage coin={coin} />
+          </TileClass>
+        );
+      }}
     </AppContext.Consumer>
-  )
+  );
 }
